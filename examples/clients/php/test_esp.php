ace2e97307464b561df8e983a7ce4c788a625179
Base Pr1me <tracey@traceyemery.com>
2017-07-24 11:08:59 -0600

    Update test_esp.php

--- examples/clients/php/test_esp.php
+++ examples/clients/php/test_esp.php
@@ -1,50 +1,50 @@
 <?php
-$address = gethostbyname("localhost");
-$port = 50020;
 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$subscribe = "~~~subscribe{{name,\"metar_slp\"},{devices{device{\"data_esp\",\"Password\"}}}}\n";
+$host = "10.0.100.5";
+$port = "50000";
 
-if ($socket === false) {
-	echo "socket_create failed";
+$url = "ftp://tgftp.nws.noaa.gov/data/observations/metar/stations/KTWF.TXT";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+if ((preg_match("/SLP[0-9]+/", curl_exec($ch), $content)) === false) {
+        die("");
+} else {
+
+        if (count($content) == 0) {
+                die("");
+        }
+        $slp = str_replace("SLP", "", $content[0]);
+        if ($slp > 500) {
+                $slp += 9000;
+        } else {
+                $slp += 10000;
+        }
+
+        $slp /= 10;
+
+        if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+                die("");
+        }
+
+        if (socket_connect($sock, $host, $port) === false) {
+                die("");
+        }
+        $msg_array = array('stationid' => 'weather_station', 'key' => '3b5912b9015c', 'slp' => $slp * 1.0);
+        $msg_json = json_encode($msg_array);
+        $msg_json .= "}";
+
+        $slength = strlen($subscribe);
+        $jlength = strlen($msg_json);
+
+        socket_write($sock, $subscribe, $slength);
+        sleep(1);
+        socket_write($sock, $msg_json, $jlength);
+
+        sleep(1);
+        socket_close($sock);
+
 }
-
-$result = socket_connect($socket, $address, $port);
-
-
-$sub_packet = '~~~subscribe{{name,"datatest"},{devices{device{"data_esp","ExE101@89#sp"}}}}';
-
-
-
-//$sub_packet = '~~~subscribe{{name,ttest},{devices{device{data_xbee,Pass1234}}}}';
-//$sub_packet = '~~~subscribe{{name,"datatest"},{devices{device{"data_xbee","DxB81^62%fg"}}}}';
-
-//$sub_packet = '~~~subscribe{{name,"ttest"},{devices{device{"data_xbee","Pass1234"}}}}';
-socket_write($socket, $sub_packet, strlen($sub_packet));
-echo "sleep 1\n";
-sleep(1);
-
-echo "send at\n";
-
-/*$at_packet = "AT\r\n";
-
-$at_packet = "AT+CIPSEND=0,5\r\n";
-socket_write($socket, $at_packet, strlen($sub_packet));
-
-sleep(1);
-$at_packet = "hello";
-socket_write($socket, $at_packet, strlen($sub_packet));
-sleep(1);*/
-
-
-
-$at_packet = "AT+CIPSTO?\r\n";
-socket_write($socket, $at_packet, strlen($sub_packet));
-
-echo "wait\n";
-$out = '';
-
-while ($out = socket_read($socket, 2048)) {
-    echo $out;
-}
-
-socket_close($socket); 
